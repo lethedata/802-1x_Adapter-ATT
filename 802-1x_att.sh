dc859dd6f24bd49e5511268675698e9ba738ad6f
lethedata <echo@lethedata.com>
2024-04-22 09:15:51 -0500

    Initial Commit

---
+++ 802-1x_att.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+# ATT 802.1x Bridge Adapter Setup Script
+# Created: 2024-04-20
+
+###
+## Stage 0: Set Variables
+###
+export OPENSSL_CONF=/opt/att/openssl.cnf
+WPA_CONF=
+PORT_ISP=
+PORT_1=
+PORT_2=
+BRIDGE_NAME=br0
+
+###
+# Stage 1: Enable 802.1x Redirection on WAN Port
+###
+/usr/bin/ebtables -t nat -A PREROUTING -i $PORT_ISP -p 0x888e --log-level 6 -j redirect
+/usr/bin/ebtables -t nat -A POSTROUTING -o $PORT_ISP -p 0x888e --log-level 6 -j snat --to-src `awk --field-separator '["#]' '/identity=/ { print $2}' "$WPA_CONF/wpa_supplicant.conf"`
+
+###
+# Stage 2: Configure Bridge and enable WAN port
+###
+/usr/bin/ip link add name $BRIDGE_NAME type bridge group_fwd_mask 8
+/usr/bin/ip link set $BRIDGE_NAME multicast off
+/usr/bin/ip link set $PORT_ISP master $BRIDGE_NAME
+/usr/bin/ip link set $PORT_1 master $BRIDGE_NAME
+/usr/bin/ip link set $PORT_2 master $BRIDGE_NAME
+/usr/bin/ip link set master $BRIDGE_NAME
+/usr/bin/ip link set $PORT_ISP up
+/usr/bin/ip link set $BRIDGE_NAME up
+
+###
+# Stage 3: 802.1x Authentication
+###
+wpa_supplicant -B -Dwired -i $BRIDGE_NAME -c "$WPA_CONF/wpa_supplicant.conf" -P/var/run/wpa_supplicant.pid
+sleep 10
+wpa_cli logon
+sleep 3
+
+###
+# Stage 4: Enable additional ports
+###
+ip link set dev $PORT_1 up
+ip link set dev $PORT_2 up
---
+++ README.md
@@ -0,0 +1,24 @@
+# AT&T 802.1x Bridge Adapter
+
+This script creates a 3 port Linux bridge allowing 802.1x authentication to be offloaded from one device (ie router) to a separate device (the bridge).
+
+Note: This script was written for use on Void Linux.
+
+## Usage
+1) Modify 802-1x_att.sh variables
+2) replace all lines in rc.local with path to script
+3) Disable ipv6 via kernel flag ipv6.disable=1
+4) Disable dhcpcd, sshd, and ntpd services
+
+
+## Q&A
+- Recommended install?
+  - Install to `/opt/att` with certs and wpa_suppicant.conf placed in a subfolder.
+- Does the bridge handle MAC Spoofing?
+  - No, devices placed behind the bridge still require MAC spoofing.
+- Why 3 ports?
+  - Three ports enables easy swapping between active-passive devices using the same MAC address.
+- Why disable ssh?
+  - The bridge device will not receive an IP address and basically acts completely transparent. The idea is to setup the bridge and never touch it.
+- Can I add additional lines to rc.conf?
+  - Technically yes however be careful not to add commands that conflict with the script.
