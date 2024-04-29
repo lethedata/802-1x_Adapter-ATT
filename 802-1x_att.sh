e0ce6379f63400294b9ac75a4b8ba822eca8dca0
lethedata <echo@lethedata.com>
2024-04-29 14:24:04 -0500

    remove full path to commands

--- 802-1x_att.sh
+++ 802-1x_att.sh
@@ -15,20 +15,20 @@ BRIDGE_NAME=br0
 ###
 # Stage 1: Enable 802.1x Redirection on WAN Port
 ###
-/usr/bin/ebtables -t nat -A PREROUTING -i $PORT_ISP -p 0x888e --log-level 6 -j redirect
-/usr/bin/ebtables -t nat -A POSTROUTING -o $PORT_ISP -p 0x888e --log-level 6 -j snat --to-src `awk --field-separator '["#]' '/identity=/ { print $2}' "$WPA_CONF_PATH/wpa_supplicant.conf"`
+ebtables -t nat -A PREROUTING -i $PORT_ISP -p 0x888e --log-level 6 -j redirect
+ebtables -t nat -A POSTROUTING -o $PORT_ISP -p 0x888e --log-level 6 -j snat --to-src `awk --field-separator '["#]' '/identity=/ { print $2}' "$WPA_CONF_PATH/wpa_supplicant.conf"`
 
 ###
 # Stage 2: Configure Bridge and enable WAN port
 ###
-/usr/bin/ip link add name $BRIDGE_NAME type bridge group_fwd_mask 8
-/usr/bin/ip link set $BRIDGE_NAME multicast off
-/usr/bin/ip link set $PORT_ISP master $BRIDGE_NAME
-/usr/bin/ip link set $PORT_1 master $BRIDGE_NAME
-/usr/bin/ip link set $PORT_2 master $BRIDGE_NAME
-/usr/bin/ip link set master $BRIDGE_NAME
-/usr/bin/ip link set $PORT_ISP up
-/usr/bin/ip link set $BRIDGE_NAME up
+ip link add name $BRIDGE_NAME type bridge group_fwd_mask 8
+ip link set $BRIDGE_NAME multicast off
+ip link set $PORT_ISP master $BRIDGE_NAME
+ip link set $PORT_1 master $BRIDGE_NAME
+ip link set $PORT_2 master $BRIDGE_NAME
+ip link set master $BRIDGE_NAME
+ip link set $PORT_ISP up
+ip link set $BRIDGE_NAME up
 
 ###
 # Stage 3: 802.1x Authentication
